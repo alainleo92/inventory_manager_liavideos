16478a14ee192d25d16ebdf50be6818183f271a0
Alain Rodriguez Acosta <alainleo92@gmail.com>
2024-02-20 07:09:23 -0500

    Se modifica el archivo base y el archivo admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import InventoryItem, Category, Order, Evento
 
-admin.site.register(InventoryItem)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'category', 'description', 'localitation')
+    list_filter = ('category',)
+
+
+admin.site.register(InventoryItem, ItemAdmin)
 admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(Evento)
