c2e897f3eae3123006854c8b8ea5e631899eaa64
Alain Rodriguez Acosta <alainleo92@gmail.com>
2024-03-01 08:05:23 -0500

    Se modifica la estructura del proyecto para una mejor comprension, se agregan parametros a url de orders, events

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from .views import Eventos, Check_Evento, EditEvento, DeleteEvento
 
 urlpatterns = [
     path('events/', Eventos.as_view(), name='events'),
-    path('event/<int:event_id>', Check_Evento.as_view(), name='event'),
-    path('edit-event/<int:pk>', EditEvento.as_view(), name='edit-event'),
-    path('delete-event/<int:pk>', DeleteEvento.as_view(), name='delete-event'),
+    path('events/event/<int:event_id>', Check_Evento.as_view(), name='event'),
+    path('events/edit-event/<int:pk>', EditEvento.as_view(), name='edit-event'),
+    path('events/delete-event/<int:pk>', DeleteEvento.as_view(), name='delete-event'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('user/signup/', SignUpView.as_view(), name='signup'),
+    path('user/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('user/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
